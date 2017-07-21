3e8a055113e10dc0fe085f65f98b533a10887349
Taro Furuya <taro.furuya@gmail.com>
2017-07-21 23:58:59 +0900

    initial

---
+++ Assets/CubeController_.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + new Vector3(0, 0.01f, 0);
+	}
+}
---
+++ Assets/NewPlayModeTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+public class NewPlayModeTest {
+
+	[Test]
+	public void NewPlayModeTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	[SetUp]
+	public void SetUp()
+	{
+		Debug.logger.logEnabled = false;
+	}
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator NewPlayModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		SceneManager.LoadScene("Test1", LoadSceneMode.Additive);
+		yield return null;
+		var cube = GameObject.Find("Cube");
+		Assert.IsNotNull(cube);
+		yield return null;
+		LogAssert.NoUnexpectedReceived();
+	}
+
+	[Test]
+	public void LogAssertEample()
+	{
+	}
+}
